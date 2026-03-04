1202dcfe09b6160fabc58804bb876e9f83488830
Ssebaleke John <jsvico100@gmail.com>
2026-03-04 13:48:01 +0300

    Fix settings: add TEMPLATES for Django admin

--- restuarant/settings.py
+++ restuarant/settings.py
@@ -35,6 +35,23 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = "restuarant.urls"
+
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],  # using app templates
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
+    },
+]
+
 WSGI_APPLICATION = "restuarant.wsgi.application"
 
 DATABASES = {
