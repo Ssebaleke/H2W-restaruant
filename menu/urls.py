fb600c45e94ef0a9305da875c2bb1792c554cec9
Ssebaleke John <jsvico100@gmail.com>
2026-03-06 17:20:32 +0300

    menu2

--- menu/urls.py
+++ menu/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import view_menu
+from .views import view_menu, menu_pdf_view
 
 urlpatterns = [
     path("menu/", view_menu, name="view_menu"),
+    path("menu/pdf/", menu_pdf_view, name="menu_pdf_view"),
 ]
--- menu/views.py
+++ menu/views.py
@@ -1,12 +1,19 @@
+from django.http import FileResponse, Http404
 from django.shortcuts import render
 from .models import MenuPDF
 
-def view_menu(request):
 
+def view_menu(request):
+    menu = MenuPDF.objects.filter(is_active=True).first()
+    return render(request, "menu/view_menu.html", {"menu": menu})
+
+
+def menu_pdf_view(request):
     menu = MenuPDF.objects.filter(is_active=True).first()
 
-    return render(
-        request,
-        "menu/view_menu.html",
-        {"menu": menu}
-    )
+    if not menu or not menu.pdf:
+        raise Http404("No active menu PDF found.")
+
+    response = FileResponse(menu.pdf.open("rb"), content_type="application/pdf")
+    response["Content-Disposition"] = 'inline; filename="menu.pdf"'
+    return response
